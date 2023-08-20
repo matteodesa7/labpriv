80ed9c04da942a685bc32012c67dcf17182a4073
MarcelloCap <capobianco.1942389@studenti.uniroma1.it>
2023-08-20 17:13:11 +0200

    Altre cose

--- labpriv-main(1)/labpriv-main/labpriv-main(1)/labpriv-main/ProgettoAssi V1/config/passport-setup.js
+++ labpriv-main(1)/labpriv-main/labpriv-main(1)/labpriv-main/ProgettoAssi V1/config/passport-setup.js
@@ -3,10 +3,18 @@ const GoogleStrategy = require("passport-google-oauth20");
 const keys = require('./keys');
 const bcrypt=require('bcrypt');
 
+
+const user = {
+    email: "",
+    nome: ""
+  };
+passport.serializeUser((user,done)=>{
+    done(null,user.nome,user.email);
+});
 passport.use(
     new GoogleStrategy({
         // L'URL a cui verrai reindirizzato dopo l'autenticazione
-        callbackURL: "http://localhost:8000/auth/google/redirect",
+        callbackURL: "/auth/google/redirect",
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken,refreshToken,profile,done) => {
@@ -14,9 +22,11 @@ passport.use(
 
         const email = profile.emails[0].value;
         const nome=profile.displayName;
+        
+        user.email=email;
+        user.nome=nome;
 
-       
-        console.log("passport callback function");
-        console.log(nome,email);
+
+        done(null,user);
     })
 );
