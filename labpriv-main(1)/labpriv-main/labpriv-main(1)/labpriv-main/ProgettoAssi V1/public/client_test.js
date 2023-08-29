c39c4f0a333f3ca997740ca9cfc54be2f6567c51
MarcelloCap <capobianco.1942389@studenti.uniroma1.it>
2023-08-29 19:53:32 +0200

    test completati

--- labpriv-main(1)/labpriv-main/labpriv-main(1)/labpriv-main/ProgettoAssi V1/public/client_test.js
+++ labpriv-main(1)/labpriv-main/labpriv-main(1)/labpriv-main/ProgettoAssi V1/public/client_test.js
@@ -1,8 +1,6 @@
 var assert=require('assert');
-describe('TestLogin',function(){
-
+describe('Test Login e Registrazione',function(){
     describe('Test BadLogin',function(){
-
         it('Dovrebbe dare un messaggio di email non presente',function(){
             var result=checkBadLogIn('email');
             assert.equal(result,'emailerror');
@@ -22,12 +20,30 @@ describe('TestLogin',function(){
         var result=checkBadLogIn('blocked');
         assert.equal(result,'blockederror');
       });
-
-
-
-
     });
 
+
+    describe('Test Registered',function(){
+      it('Dovrebbe dare un messaggio di email non presente',function(){
+          var result=checkRegistered('tobeConfirmed');
+          assert.equal(result,'mailconfirmed');
+      });
+
+      it('Dovrebbe dare un messaggio di password sbagliata',function(){
+        var result=checkRegistered('false');
+        assert.equal(result,'falseerror');
+    });
+
+    it('Dovrebbe dare un messaggio di email non confermata',function(){
+      var result=checkBadLogIn('notConfirmed');
+      assert.equal(result,'ncerror');
+    });
+
+    it('Dovrebbe dare un messaggio di account bloccato',function(){
+      var result=checkBadLogIn('blocked');
+      assert.equal(result,'blockederror');
+    });
+  });
 });
 
 
@@ -59,4 +75,35 @@ function checkBadLogIn(Test){
   }
 function removeL(){
     return;
+}
+function removeR(){
+  return;
+}
+function checkRegistered(test){
+    switch(test){
+      case 'tobeConfirmed':
+        return 'mailconfirmed';
+        break;
+      case 'false':
+        return 'falseerror';
+        break;
+      case 'primary':
+        return 'primaryerror';
+        break;
+      case 'badpass':
+        return 'badpasserror';
+        break;
+      case 'noName':
+        return 'noNameerror';
+          break;
+      case 'noMail':
+      return 'noMailerror';
+          break; 
+      case 'true':
+      return 'true';
+          break;  
+      default:
+        //utente non registrato
+        break;
+    }
 }
